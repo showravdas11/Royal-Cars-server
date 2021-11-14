ac9f55f2f17621d3a41110231e8c39d753231592
showravdas11 <showravdas11@gmail.com>
2021-11-14 21:57:20 +0600

    done

--- index.js
+++ index.js
@@ -88,6 +88,17 @@ async function run() {
             res.json(result)
         })
 
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({ admin: isAdmin })
+        })
+
 
         app.put('/users/admin', async (req, res) => {
             const user = req.body
